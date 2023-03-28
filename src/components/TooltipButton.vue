801dafc9a1055ae598cea6b635e229bac9e12904
Šimon Macek <simon.macek@proficio.cz>
2023-03-28 15:06:31 +0200

    components: add tooltip-button with bs icons

---
+++ src/components/TooltipButton.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import config from '../../app.config.toml';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TooltipButton',
+
+  setup() {
+    return {
+      lightGrayColor: config.lightGrayColor,
+    };
+  },
+});
+</script>
+
+<template>
+  <q-btn
+    icon="bi-question-circle-fill"
+    flat
+    unelevated
+    round
+    :stretch="false"
+    :style="{ backgroundColor: lightGrayColor }"
+  >
+    <q-tooltip> Some text as content of Tooltip </q-tooltip>
+  </q-btn>
+</template>
