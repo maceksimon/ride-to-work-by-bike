533a4f238e2d3783a923a655856d3e4e433f7029
Šimon Macek <simon.macek@proficio.cz>
2023-03-30 11:04:23 +0200

    component: add swiper lib + card styling

--- src/components/CardNews.vue
+++ src/components/CardNews.vue
@@ -12,6 +12,62 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div>{{ $t(`slider.card.${props.card.id}.title`) }}</div>
-  <div>{{ $d(props.card.datePublished, 'short') }}</div>
+  <article class="card-news font-lexend text-bold">
+    <div class="card-news__image">
+      <a href="">
+        <q-img ratio="1.33" src="https://via.placeholder.com/400x300"></q-img>
+      </a>
+    </div>
+    <div class="card-news__content">
+      <a href="">
+        <h2 class="card-news__title q-ma-none text-h6 text-weight-bold">
+          {{ $t(`slider.card.${props.card.id}.title`) }}
+        </h2>
+      </a>
+      <time
+        v-if="props.card.datePublished"
+        class="card-news__date text-weight-regular"
+        :datetime="props.card.datePublished.toString()"
+      >
+        {{ $d(props.card.datePublished, 'short') }}
+      </time>
+    </div>
+  </article>
 </template>
+
+<style lang="scss" scoped>
+.card-news {
+  box-sizing: border-box;
+  border: 1px solid $grey-4;
+  background-color: #fff;
+  border-radius: 6px 6px 24px 24px;
+  overflow: hidden;
+  height: 100%;
+
+  &__image {
+    max-width: 100%;
+  }
+
+  &__content {
+    padding: 16px;
+
+    a {
+      text-decoration: none;
+      color: #000;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    .card-news__title {
+      line-height: 1.2;
+    }
+    .card-news__date {
+      display: inline-block;
+      margin-top: 8px;
+      color: $grey-5;
+    }
+  }
+}
+</style>
--- src/components/SliderCards.vue
+++ src/components/SliderCards.vue
@@ -1,35 +1,112 @@
-<script setup>
+<script lang="ts" setup>
 import { defineComponent } from 'vue';
 import CardNews from './CardNews.vue';
+// Import Swiper Vue.js components
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 
 defineComponent({
   components: {
     CardNews,
+    Swiper,
+    SwiperSlide,
   },
 });
 
 const cards = [
   {
-    id: 'one',
+    id: '1',
     datePublished: new Date('2022-09-27T13:42:44+00:00'),
   },
   {
-    id: 'two',
+    id: '2',
     datePublished: new Date('2022-09-22T13:42:44+00:00'),
   },
   {
-    id: 'three',
+    id: '3',
     datePublished: new Date('2022-09-14T13:42:44+00:00'),
   },
   {
-    id: 'four',
+    id: '4',
+    datePublished: new Date('2022-09-14T13:42:44+00:00'),
+  },
+  {
+    id: '3',
+    datePublished: new Date('2022-09-14T13:42:44+00:00'),
+  },
+  {
+    id: '4',
     datePublished: new Date('2022-09-14T13:42:44+00:00'),
   },
 ];
 </script>
 
 <template>
-  <div v-for="card in cards" :key="card.id">
-    <card-news :card="card"></card-news>
+  <div class="relative-position">
+    <swiper
+      :modules="[Navigation]"
+      navigation
+      :slides-per-view="4"
+      :space-between="24"
+    >
+      <swiper-slide v-for="card in cards" :key="card.id" class="swiper-slide">
+        <card-news :card="card"></card-news>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
+
+<style lang="scss">
+.swiper {
+  padding-bottom: 64px;
+}
+.swiper-slide {
+  height: auto;
+}
+.swiper-button-next,
+.swiper-button-prev {
+  border-radius: 9999px;
+  top: auto;
+  bottom: 0;
+  width: 40px;
+  height: 40px;
+  color: $grey-10;
+  background-color: #fff;
+  border: 1px solid $grey-10;
+
+  &:after {
+    width: 32px;
+    height: 32px;
+    content: '';
+  }
+
+  &.swiper-button-disabled {
+    opacity: 1;
+    border-color: $grey-5;
+
+    &:after {
+      opacity: 0.35;
+    }
+  }
+}
+.swiper-button-prev {
+  left: auto;
+  right: 64px;
+
+  &:after {
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 16 16'%3E%3Cpath fill='currentColor' fill-rule='evenodd' d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z'/%3E%3C/svg%3E");
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+}
+.swiper-button-next {
+  left: auto;
+  right: 0;
+
+  &:after {
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 16 16'%3E%3Cpath fill='currentColor' fill-rule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z'/%3E%3C/svg%3E");
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+}
+</style>
