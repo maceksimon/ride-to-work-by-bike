795579f9c25dfc6a00c5b4ed428ad301f55bba31
Šimon Macek <simon.macek@proficio.cz>
2023-03-31 10:41:21 +0200

    components: merge language-switcher component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,14 @@
 <script lang="ts" setup>
-import { defineComponent } from 'vue';
 import config from '../../app.config.toml';
-import LanguageSwitcher from 'components/LanguageSwitcher.vue';
+import { inject } from 'vue';
+import { useRouteQuery } from '@vueuse/router';
 
 const lightGrayColor = config.lightGrayColor;
 
-defineComponent({
-  components: {
-    LanguageSwitcher,
-  },
-});
+const lang = useRouteQuery('lang');
+
+const log: any = inject('vuejs3-logger');
+log.debug(lang.value);
 </script>
 
 <template>
@@ -34,8 +33,56 @@ defineComponent({
           <q-tooltip>{{ $t('tooltip') }}</q-tooltip>
         </q-btn>
 
-        <language-switcher class="q-ml-xl"></language-switcher>
+        <div class="language-switcher q-ml-xl">
+          <q-btn
+            label="EN"
+            unelevated
+            round
+            color="secondary"
+            text-color="white"
+            class="language-switcher--en"
+            :class="{ 'is-active': $i18n.locale === 'en-US' }"
+            @click.prevent="
+              {
+                $i18n.locale = 'en-US';
+                lang = 'en';
+              }
+            "
+          />
+          <q-btn
+            label="CZ"
+            unelevated
+            round
+            color="secondary"
+            text-color="white"
+            class="language-switcher--cs"
+            :class="{ 'is-active': $i18n.locale === 'cs-CZ' }"
+            @click.prevent="
+              {
+                $i18n.locale = 'cs-CZ';
+                lang = 'cs';
+              }
+            "
+          />
+        </div>
       </q-toolbar>
     </div>
   </q-header>
 </template>
+
+<style lang="scss">
+.language-switcher {
+  display: flex;
+  flex-direction: row-reverse;
+  flex-wrap: nowrap;
+
+  > *:not(:first-child) {
+    margin-right: -8px;
+  }
+
+  .q-btn.is-active {
+    z-index: 10;
+    background-color: $primary !important;
+  }
+}
+</style>
